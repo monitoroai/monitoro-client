2b9c84a4bef6951d1abc0c8b87fece27f09b226c
Charles Ashby <charlesa.l@hotmail.com>
2020-05-06 22:14:36 -0700

    chore(requirements): Updated requirements.txt (#2)
    * chore(requirements): Updated requirements.txt
    
    * chore(requirements): Updated requirements.txt

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,10 @@
 from fastapi.testclient import TestClient
+from pathlib import Path
+
+test_files_directory = f"{Path.cwd()}/tests/files/"
+Path(test_files_directory).mkdir(parents=True, exist_ok=True)
+file = Path(test_files_directory) / "access.log"
+Path(file).touch()
 
 from main import app
 
